221c33fc52344ace7507eb9ec412f4fe1c9cadb5
Casey Hartnett <chartnett@cvent.com>
2015-10-04 16:30:16 -0400

    Add get_heroes and get_item_details functions.

---
+++ R/misc_data.R
@@ -0,0 +1,31 @@
+
+#' Retrieve current heroes details
+#'
+#' @param api list from get_api_details
+#'
+#' @return api results in a list
+#' @export
+#'
+#' @examples
+#' get_heroes()
+get_heroes <- function(api = get_api_details()) {
+  request = paste0(api$url, "IEconDOTA2_570/GetHeroes/v0001/?key=", api$key, "&language=en_us")
+  data = rjson::fromJSON(RCurl::getURL(request))
+  return(data)
+}
+
+#' Retrieve details about a item
+#'
+#' @param item_id a single item id
+#' @param api list from get_api_details
+#'
+#' @return api results in a list
+#' @export
+#'
+#' @examples
+#' gget_item_details(item_id)
+get_item_details <- function(item_id, api = get_api_details()) {
+  request = paste0(api$url, "IEconDOTA2_570/GetGameItems/V001/?key=", api$key, "&language=en_us")
+  data = rjson::fromJSON(RCurl::getURL(request))
+  return(data)
+}
